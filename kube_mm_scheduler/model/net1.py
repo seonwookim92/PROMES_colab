7afe66a8a30b15b6029819de5eead2f2766b342b
Seonwoo Kim <88951594+seki5405@users.noreply.github.com>
2023-05-19 13:17:04 -0600

    Add pretrained, freeze, base_path

--- kube_mm_scheduler/model/net1.py
+++ kube_mm_scheduler/model/net1.py
@@ -2,8 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+import os, sys
+base_path = os.getcwd()
+print(f"Base Path: {base_path}")
+sys.path.append(base_path)
+
 class Net1(nn.Module):
-    def __init__(self, pretrained):
+    def __init__(self, pretrained, freeze):
         super(Net1, self).__init__()
         self.fc1 = nn.Linear(12, 64)
         self.fc2 = nn.Linear(64, 64)
@@ -11,16 +16,16 @@ class Net1(nn.Module):
 
         if pretrained:
             # Load pretrained weights
-            self.load_state_dict(torch.load('net1.pt'))
+            self.load_state_dict(os.path.join(base_path, "kube_mm_scheduler", "weight", "net1.pt"))
 
-        #     # Freeze those weights
-        #     for param in self.parameters():
-        #         param.requires_grad = False
-
-        # else:
-        #     # Unfreeze net5
-        #     for param in self.net5.parameters():
-        #         param.requires_grad = True
+        if freeze:
+            # Freeze those weights
+            for param in self.parameters():
+                param.requires_grad = False
+        else:
+            # Unfreeze net5
+            for param in self.net5.parameters():
+                param.requires_grad = True
 
     def forward(self, x1, x2):
         x = torch.cat((x1, x2), dim=1)
